9d197acd032810c755218f79dd58e9d37b784d2a
akashmapari <akashmapari85@gmail.com>
2023-12-17 21:44:50 +0530

    connectivity

---
+++ bootconnectivity/src/test/java/com/boot/conectivity/BootconnectivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.conectivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootconnectivityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
